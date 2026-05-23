8c364d92973b284d2d6fd938ef730283028be39f
grish-ka <grishka.krot@gmail.com>
2026-05-23 09:45:45 +0100

    Initialize go.mod and main.go files

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/grish-ka/go-stock
+
+go 1.26.3
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("go-stock is still work in progress,\nthis is it for know while I plan stuff")
+}
